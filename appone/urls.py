5624380be19295c6c99a38551b5d10b49269cadd
phevabb <phevab1@gmail.com>
2025-05-16 20:06:09 +0000

    init

---
+++ appone/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.service, name='service'),
+    path('contact_us/', views.contact, name='contact'),
+    path('pojects/', views.project, name='project'),
+]
---
+++ appone/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def project(request):
+    return render(request, 'project.html')
