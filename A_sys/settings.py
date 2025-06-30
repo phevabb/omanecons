4bd8fb2873fa12bfa58c12e36b6e1d1404c2426c
phevab <phevab1@gmail.com>
2025-06-30 08:17:54 +0000

    d-true

--- A_sys/settings.py
+++ A_sys/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5r0!-zlp$c)d8lrsr#o&1c&k4fnx)xarx=m(n@$d@m9(e1uodh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
