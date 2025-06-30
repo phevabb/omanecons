f9746d5cceba1173ed5409b6f821a7bff637909b
phevab <phevab1@gmail.com>
2025-06-30 13:21:30 +0000

    cloud image update

--- A_sys/urls.py
+++ A_sys/urls.py
@@ -48,7 +48,6 @@ urlpatterns = [
     path("robots.txt", robots_txt),
 
 ]
-if settings.DEBUG == False:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
