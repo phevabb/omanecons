57458f7b6ff53b7bb13542977d7e3a5456ff4136
phevab <phevab1@gmail.com>
2025-07-09 02:22:21 +0000

    updates_1

--- bk.py
+++ bk.py
@@ -1,16 +1,23 @@
 import os
+import django
+
+# Set the Django settings module (replace 'yourproject' with your actual project name)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A_sys.settings')
+django.setup()
+
+# Now import models AFTER setup
+from appone.models import Engine
 from django.core.files import File
-from appone.models import Logistic  # Replace 'yourapp' with the actual app name
 
 # Path to the folder with images
-image_folder = "/home/ato/Desktop/django_projects/omane/photos/KKA_Logistics"
+image_folder = r"C:\Users\ato\Desktop\dj\cious\KKA_Engineering"
+
 
 # Loop through each file in the folder
 for filename in os.listdir(image_folder):
     if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         image_path = os.path.join(image_folder, filename)
         with open(image_path, 'rb') as f:
-            # Create Handyman object with image file
-            L = Logistic()
-            L.image.save(filename, File(f), save=True)
+            E = Engine()
+            E.image.save(filename, File(f), save=True)
             print(f"Uploaded: {filename}")
